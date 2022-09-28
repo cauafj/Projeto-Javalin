fb8d58c874ed5248be86a950ab84a607b085ea16
cauafj <cauafjz@gmail.com>
2022-09-28 13:51:14 -0300

    commit do VIega

--- src/main/java/packmvn/App.java
+++ src/main/java/packmvn/App.java
@@ -1,13 +1,14 @@
 package packmvn;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
