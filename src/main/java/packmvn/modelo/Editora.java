0ec322a995c64c645adb3ebde9adddd9f039331c
VitorEMSB <vitoremmanuelb@gmail.com>
2022-10-20 15:46:51 -0300

    Finalizando DAO de cadastrar considerando id com PK

--- src/main/java/packmvn/modelo/Editora.java
+++ src/main/java/packmvn/modelo/Editora.java
@@ -4,12 +4,20 @@ public class Editora {
     private String nome;
     private String cnpj;
     private String email;
+    private int id;
+
     public Editora(String nome, String cnpj, String email) {
         super();
         this.nome = nome;
         this.cnpj = cnpj;
         this.email = email;
     }
+    
+    public Editora(int id, String nome, String cnpj, String email) {
+        this(nome, cnpj,email);
+        this.id = id; 
+    } 
+    
     public String getNome() {
         return nome;
     }
@@ -27,5 +35,11 @@ public class Editora {
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- src/main/java/packmvn/persistencia/EditoraDao.java
+++ src/main/java/packmvn/persistencia/EditoraDao.java
@@ -18,39 +18,70 @@ public class EditoraDao implements DAO<Editora> {
         String sql = "INSERT INTO Editora (nome, cnpj, email) VALUES (?,?,?)";
 
         try (Connection con = Conexao.getConexao();
-                PreparedStatement pstm = con.prepareStatement(sql)) {
+                PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             pstm.setString(1, e.getNome());
             pstm.setString(2, e.getCnpj());
             pstm.setString(3, e.getEmail());
+            ResultSet rs = pstm.executeQuery();
+            rs.next();
+            e.setId(rs.getInt("id"));
+            rs.close();
+        } catch (SQLException exp) {
+            System.out.println(exp.getMessage());
+            exp.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void alterar(Editora e) {
+        String sql = "UPDATE editora SET cnpj = ?, nome = ?, email = ? WHERE id = ?";
+        try (Connection con = Conexao.getConexao();
+                PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setString(1, e.getCnpj());
+            pstm.setString(2, e.getNome());
+            pstm.setString(3, e.getEmail());
+            pstm.setInt(4, e.getId());
+            
             pstm.executeUpdate();
         } catch (SQLException exp) {
             System.out.println(exp.getMessage());
             exp.printStackTrace();
         }
-    
+
     }
 
     @Override
     public void excluir(int id) {
-        // TODO Auto-generated method stub
-        
+        String sql = "DELETE FROM Editora  WHERE id = ? ";
+        try (Connection con = Conexao.getConexao();
+                PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setInt(1, id);
+
+            pstm.executeUpdate();
+        } catch (SQLException exp) {
+            System.out.println(exp.getMessage());
+            exp.printStackTrace();
+        }
+
     }
 
     @Override
     public List<Editora> listar(int limit, int offset) {
-        String sql = "SELECT nome, cnpj, email  FROM Editora  LIMIT ? OFFSET ?";
+        String sql = "SELECT id, nome, cnpj, email  FROM Editora  LIMIT ? OFFSET ?";
         ArrayList<Editora> editoras = new ArrayList<>();
         try (Connection con = Conexao.getConexao();
-             PreparedStatement pstm = con.prepareStatement(sql);) {
-            pstm.setInt(1,limit);
-            pstm.setInt(2,offset);
-            
+                PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setInt(1, limit);
+            pstm.setInt(2, offset);
+
             ResultSet rs = pstm.executeQuery();
-            while(rs.next()) {
-                Editora e = new Editora(rs.getString("nome"),
-                        rs.getString("cnpj"),rs.getString("email"));
+            while (rs.next()) {
+                Editora e = new Editora(rs.getInt("id"), rs.getString("nome"),
+                        rs.getString("cnpj"), rs.getString("email"));
                 editoras.add(e);
             }
+            rs.close();
         } catch (SQLException exp) {
             System.out.println(exp.getMessage());
             exp.printStackTrace();
@@ -59,46 +90,24 @@ public class EditoraDao implements DAO<Editora> {
     }
 
     @Override
-    public void alterar(Editora e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
     public Editora buscar(int id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public void excluir(String cnpj) {
-        String sql = "DELETE FROM Editora  WHERE cnpj = ? ";
-        try (Connection con = Conexao.getConexao();
-             PreparedStatement pstm = con.prepareStatement(sql);) {
-            pstm.setString(1,cnpj);
-            
-            pstm.executeUpdate();
-        } catch (SQLException exp) {
-            System.out.println(exp.getMessage());
-            exp.printStackTrace();
-        }
-    }
-
-    public Editora buscar(String cnpj) {
-        String sql = "SELECT nome, cnpj, email  FROM Editora  WHERE cnpj= ? ";
+        String sql = "SELECT id, nome, cnpj, email  FROM Editora  WHERE id = ? ";
         Editora edi = null;
         try (Connection con = Conexao.getConexao();
-             PreparedStatement pstm = con.prepareStatement(sql);) {
-            pstm.setString(1,cnpj);
-            
+                PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setInt(1, id);
+
             ResultSet rs = pstm.executeQuery();
             rs.next();
-            edi = new Editora(rs.getString("nome"),
-                       rs.getString("cnpj"),rs.getString("email"));
+            edi = new Editora(rs.getInt("id"), rs.getString("nome"),
+                    rs.getString("cnpj"), rs.getString("email"));
+            rs.close();
         } catch (SQLException exp) {
             System.out.println(exp.getMessage());
             exp.printStackTrace();
         }
+
         return edi;
     }
-   
+
 }
